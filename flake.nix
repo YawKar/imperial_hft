c57cefb7e95a478b2a8f6fb01cae0c9911225061
YawKar <yawkarpov@gmail.com>
2024-08-03 23:33:39 +0300

    add flake with gcc14

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "imperial_hft";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { /*self,*/ nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        mkShell' = pkgs.mkShell.override {
+          stdenv = pkgs.gcc14Stdenv;
+        };
+      in
+      {
+        devShells.default = mkShell' {
+          buildInputs = with pkgs; [
+            gnumake
+          ];
+          shellHook = ''
+            export PS1="(imperial_hft)$PS1"
+          '';
+        };
+      }
+    );
+}
